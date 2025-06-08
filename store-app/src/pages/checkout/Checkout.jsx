2ee198af85a2fc8163d803ee59bdff7ef49225df
Ayhan KARAMAN <ayhan28ak@gmail.com>
2025-06-08 02:18:01 +0000

    feat: stepper added and checkout component design completed

--- store-app/src/pages/checkout/Checkout.jsx
+++ store-app/src/pages/checkout/Checkout.jsx
@@ -1,23 +1,91 @@
-import { Grid, Paper } from '@mui/material'
+import { Box, Button, Grid, Paper, Step, StepLabel, Stepper, Typography } from '@mui/material'
 import Info from './Info'
 import AddressForm from './AddressForm'
 import PaymentForm from './PaymentForm'
 import Review from './Review'
+import { useState } from 'react'
+import { ChevronLeftRounded, ChevronRightRounded } from "@mui/icons-material";
+
+const steps = [
+   "Teslimat Bilgileri", "Ödeme Bilgileri", "Sipariş Özeti"
+]
+
+const getStepContent = (step) => {
+   switch (step) {
+      case 0:
+         return <AddressForm />
+      case 1:
+         return <PaymentForm />
+      case 2:
+         return <Review />
+
+   }
+}
+
+
+
 const CheckoutPage = () => {
-  return (
-    <Paper>
-       <Grid container spacing={3}>
-          <Grid size={4} sx={{ p:3, borderRight:'1px solid', borderColor:'diveder'}} >
-              <Info />
-          </Grid>
-          <Grid size={8} padding={3} >
-             <AddressForm />
-             <PaymentForm />
-             <Review />
-          </Grid>
-       </Grid>
-    </Paper>
-  )
+   const [activeStep, setActiveStep] = useState(0)
+   const handlePrevious = () => {
+        setActiveStep(activeStep -1)
+   }
+   const handleNext = () => {
+       setActiveStep(activeStep + 1)
+   }
+   return (
+      <Paper>
+         <Grid container spacing={3}>
+            <Grid size={4} sx={{ p: 3, borderRight: '1px solid', borderColor: 'diveder' }} >
+               <Info />
+            </Grid>
+            <Grid size={8} padding={3} >
+               <Stepper activeStep={activeStep} sx={{ height: 40, mb: 4 }}>
+                  {
+                     steps.map((label, index) => (
+                        <Step key={index} color='primary' >
+                           <StepLabel>{label}</StepLabel>
+                        </Step>
+                     ))
+                  }
+               </Stepper>
+
+               {
+                  activeStep === steps.length
+                     ? (<Typography variant='h5' textAlign={'center'} color='primary' >Siparişinizi Aldık</Typography>)
+                     : (
+                        <>
+                           {getStepContent(activeStep)}
+                           <Box sx={[
+                              { display: 'flex', mt:4, alignItems:'flex-end' },
+                              activeStep === 0 ? { justifyContent: 'flex-end' } : { justifyContent: 'space-between' }
+                           ]}>
+                              {
+                                 activeStep !== 0 && <Button
+                                    onClick={handlePrevious}
+                                    startIcon={<ChevronLeftRounded />}
+                                    variant="contained"
+                                    color="primary"
+                                 >
+                                    Geri
+                                 </Button>
+                              }
+                              <Button
+                                 onClick={handleNext}
+                                 startIcon={<ChevronRightRounded />}
+                                 variant="contained"
+                                 color="primary"
+                                 
+                              >
+                                 İleri
+                              </Button>
+                           </Box>
+                        </>
+                     )
+               }
+            </Grid>
+         </Grid>
+      </Paper>
+   )
 }
 
 export default CheckoutPage
--- store-app/src/pages/checkout/Info.jsx
+++ store-app/src/pages/checkout/Info.jsx
@@ -1,10 +1,43 @@
+import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { baseURL } from '../../api/urls';
+import { currencyTRY } from '../../utilities/tools/tools';
 
 const Info = () => {
+  const { cart } = useSelector((state) => state.shoppingCart);
+
+  const subTotal = cart?.cartItems.reduce((total, item) =>
+    total + item.product.price * item.product.quantity, 0);
+
+  const tax = subTotal * 0.2;
+  const total = subTotal + tax;
+
   return (
-    <h1>
-      Info
-    </h1>
+    <>
+      <Typography variant='h6' fontStyle={'italic'} textAlign='center' >Sepetiniz</Typography>
+      <List>
+        {
+          cart?.cartItems.map((item, index) => (
+            <ListItem key={index} sx={{ py: 1, px: 0 }}>
+              <ListItemAvatar>
+                <Avatar variant='square' src={`${baseURL}images/${item.product.image}`} />
+              </ListItemAvatar>
+              <ListItemText primary={item.product.title} secondary={`X.${item.product.quantity}`} />
+              <Typography> {currencyTRY.format(item.product.price)} </Typography>
+            </ListItem>
+          ))
+        }
+      </List>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 4, pt:2 }}>
+        <Typography variant="h5" fontWeight={'bold'} color={{ color: "text.secondary" }}>
+          Toplam:
+        </Typography>
+        <Typography variant="h5" color={{color:'text.secondary'}} gutterBottom>
+          {currencyTRY.format(total)}
+        </Typography>
+      </Box>
+    </>
   )
 }
 
